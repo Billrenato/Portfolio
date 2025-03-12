0ecf4f1d036ad153e16ed41e47b99bbb58f24332
RENATO JUNIOR MATHIAS <118221666+Billrenato@users.noreply.github.com>
2025-03-11 21:51:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ hamburguer.addEventListener("click", () =>{
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
+
+
+window.addEventListener('scroll', function() {
+    const legendaSobre = document.querySelector('.legenda-sobre','.container');
+    const scrollTop = window.scrollY;
+    const offsetTop = legendaSobre.offsetTop;
+  
+    if (scrollTop > offsetTop - 200) {
+      legendaSobre.classList.add('animar');
+    }
+  });
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const Container = document.querySelector('.container');
+    Container.classList.add('animar');
+  });
